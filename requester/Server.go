c3448c0b59bd4730ad8a3a668afcdd7eae6b6b66
daixiaoman <2821224626@qq.com>
2019-05-31 09:57:27 +0800

    ok

--- requester/Server.go
+++ requester/Server.go
@@ -21,8 +21,8 @@ var (
 	heyServer = &http.Server{
 		Addr:           ":7654",
 		Handler:        &MyServer{},
-		ReadTimeout:    10000 * time.Microsecond,
-		WriteTimeout:   10000 * time.Microsecond,
+		ReadTimeout:    100000 * time.Microsecond,
+		WriteTimeout:   100000 * time.Microsecond,
 		MaxHeaderBytes: 1 << 30,
 	}
 	heyHandlerMap = make(map[string]HandlersFunc)
